3ef00729a9d3801cad7dc01afe060a24c1c65e28
Alexandre Belchior <alexbelchior@gmail.com>
2018-07-05 14:40:22 +0200

    translation update

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alexandre Belchior <alexbelchior@gmail.com>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['menu']                  = 'Gerenciador de acesso HTML';
---
+++ lang/pt-br/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alexandre Belchior <alexbelchior@gmail.com>
+ */
+$lang['custom_error_but']      = 'Marque esta caixa se estiver instalando um botão de erro personalizado para exibir erros de HTML';
+$lang['su_unrestricted']       = 'Super usuário irrestrito';
