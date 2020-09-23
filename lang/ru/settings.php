d8da168bfeaf8be695c508f2a8518fdf79ae5953
Aleksandr Selivanov <alexgearbox@yandex.ru>
2020-09-23 18:40:06 +0200

    translation update

---
+++ lang/ru/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
+ */
+$lang['su_unrestricted']       = 'Суперпользователь с&nbsp;неограниченным доступом';
