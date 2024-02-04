c61d5890b1e79c62b8924e9c505fc7224760aa97
mod <modex@mail.ru>
2024-02-04 06:50:42 +0100

    translation update

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -3,7 +3,9 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author mod <modex@mail.ru>
  * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  */
 $lang['encoding']              = 'utf-8';
 $lang['direction']             = 'ltr';
+$lang['menu']                  = 'Менеджер доступа к HTML';
--- lang/ru/settings.php
+++ lang/ru/settings.php
@@ -3,6 +3,8 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author mod <modex@mail.ru>
  * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  */
+$lang['custom_error_but']      = 'Установите этот флажок, если вы устанавливаете специальную кнопку для отображения ошибок HTML.';
 $lang['su_unrestricted']       = 'Суперпользователь с&nbsp;неограниченным доступом';
